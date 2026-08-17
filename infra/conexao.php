ce864f494c13bc7e61c09ef39585377c646d00a0
IncertiHenri <henrique_incerti@estudante.sesisenai.org.br>
2026-08-17 11:01:45 -0300

    add - conexão

--- infra/conexao.php
+++ infra/conexao.php
@@ -1,6 +1,18 @@
 <?php
 
+$host = "localhost";
+$usuario = "root";
+$senha = "root";
+$db = "sistema_pratos";
 
+$conn = new mysqli($host, $usuario, $senha, $db);
 
+if ($conn->connect_error) {
+    die("Falha na conexão");
+}
+
+$conn->set_charset("utf8mb4");
+
+echo "Conectado com sucesso!";
 
 ?>

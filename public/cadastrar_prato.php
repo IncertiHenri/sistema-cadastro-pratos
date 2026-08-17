12a78cd938166c5990ca743e61d5a028157bc37f
serennadias <serenna_tavares@estudante.sesisenai.org.br>
2026-08-17 11:36:28 -0300

    fix - cadastro usuarios

--- public/cadastrar_prato.php
+++ public/cadastrar_prato.php
@@ -7,9 +7,13 @@ $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
 $categoria = $_POST["categoria"];
 
+header("Location:../index.php");
+exit;
+
 echo "Nome: " . $nome . "<br>";
 echo "Descrição: " . $descricao . "<br>";
 echo "Preço: " . $preco . "<br>";
 echo "Categoria: " . $categoria . "<br>";
+=
 
 ?>
--- public/cadastrar_usuario.php
+++ public/cadastrar_usuario.php
@@ -5,7 +5,11 @@ include("../infra/conexao.php");
 $nome = $_POST["nome"];
 $email = $_POST["email"];
 
-echo "Nome: " . $nome . "<br>";
-echo "Email: " . $email . "<br>";
+$sql = "INSERT INTO usuario (nome,email) VALUES ('$nome','$email')";
+
+$conn->query($sql);
+
+header("Location:../index.php");
+exit;
 
 ?>
